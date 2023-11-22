68b34c3054fd238ea8748bc8d4f68b648b67e702
hamthelegend <hamuelxd@gmail.com>
2023-11-23 03:44:34 +0800

    Fixed incorrect texts

--- app/src/main/java/com/thebrownfoxx/petrealm/ui/screens/pets/components/AdoptDialog.kt
+++ app/src/main/java/com/thebrownfoxx/petrealm/ui/screens/pets/components/AdoptDialog.kt
@@ -1,6 +1,9 @@
 package com.thebrownfoxx.petrealm.ui.screens.pets.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,9 +11,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
 import com.thebrownfoxx.components.FilledButton
 import com.thebrownfoxx.components.TextButton
+import com.thebrownfoxx.components.VerticalSpacer
 import com.thebrownfoxx.components.extension.rememberMutableStateOf
+import com.thebrownfoxx.petrealm.ui.components.PetCard
 import com.thebrownfoxx.petrealm.ui.components.TextField
 import com.thebrownfoxx.petrealm.ui.components.indicationlessClickable
 import com.thebrownfoxx.petrealm.ui.screens.pets.state.AdoptDialogState
@@ -30,18 +36,30 @@ fun AdoptDialog(
                 focusManager?.clearFocus(true)
             },
             onDismissRequest = stateChangeListener.onCancelAdopt,
-            title = { Text(text = "Add Pet") },
+            title = { Text(text = "Adopt pet") },
             text = {
                 // The focus manager should be from inside the AlertDialog
                 focusManager = LocalFocusManager.current
-
-                TextField(
-                    label = "Owner name",
-                    value = state.ownerName,
-                    onValueChange = stateChangeListener.onOwnerNameChange,
-                    required = true,
-                    error = if (state.hasOwnerNameWarning) "Required" else null,
-                )
+                Column {
+                    PetCard(
+                        petName = state.pet.name,
+                        petAge = state.pet.age.toString(),
+                        petType = state.pet.type.name,
+                        ownerName = state.ownerName,
+                    )
+                    VerticalSpacer(height = 16.dp)
+                    TextField(
+                        label = "Owner name",
+                        value = state.ownerName,
+                        onValueChange = stateChangeListener.onOwnerNameChange,
+                        required = true,
+                    )
+                    Text(
+                        text = "Required",
+                        style = MaterialTheme.typography.labelSmall,
+                        modifier = Modifier.padding(start = 16.dp, top = 4.dp),
+                    )
+                }
             },
             dismissButton = {
                 TextButton(
@@ -49,7 +67,7 @@ fun AdoptDialog(
                     onClick = stateChangeListener.onCancelAdopt,
                 )
             },
-            confirmButton = { FilledButton(text = "Add", onClick = stateChangeListener.onAdopt) }
+            confirmButton = { FilledButton(text = "Adopt", onClick = stateChangeListener.onAdopt) }
         )
     }
 }
