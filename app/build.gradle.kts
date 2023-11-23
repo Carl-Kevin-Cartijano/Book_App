8210558dac5b611b731d4d60c21e02f180fc2ad4
hamthelegend <hamuelxd@gmail.com>
2023-11-23 16:57:20 +0800

    Upped version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.thebrownfoxx.petrealm"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
